6dbc9cfbef6b88d1cdbdfd32297f944e093bf95f
jli47 <jingna13@gmail.cmo>
2018-09-10 15:02:10 -0700

    Add user validations

--- art_share/db/migrate/20180910215930_create_users.rb
+++ art_share/db/migrate/20180910215930_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :username
-
+      t.string :username, null: false
       t.timestamps
     end
+    add_index :users, :username, uniqe: true 
   end
 end
