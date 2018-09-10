3bf48582bf95d0a7a6e4bc0bb80051d550bc2cbd
jli47 <jingna13@gmail.cmo>
2018-09-10 14:30:59 -0700

    Add User model

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -1,3 +1,31 @@
 class UsersController < ApplicationController 
   
+  def index
+    render plain: "I'm in the index action!" 
+  end 
+  
+  def create 
+    user = User.new(strong_params)
+    
+    if user.save
+      render json: user 
+    else 
+      render json: user.errors.full_messages, status: 422 
+    end 
+  end 
+  
+  def show 
+    user = User.find(params[:id])
+    
+    if user
+      render json: user 
+    else  
+      render json: user.errors.full_messages, status: 404 
+    end 
+  end 
+  
+  def strong_params 
+    params.require(:user).permit(:name, :email)
+  end 
+  
 end 
