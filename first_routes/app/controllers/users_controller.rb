6fb362f02290a5d267377d3afb123971d6573366
jli47 <jingna13@gmail.cmo>
2018-09-10 14:51:51 -0700

    Add show, update, and destroy to UsersController

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -15,17 +15,46 @@ class UsersController < ApplicationController
   end 
   
   def show 
-    user = User.find(params[:id])
+    user = User.find_by(id: id)
     
     if user
       render json: user 
     else  
-      render json: user.errors.full_messages, status: 404 
+      render json: 'User Not Found', status: 404 
     end 
+  end 
+  
+  def update
+    user = User.find_by(id: id)
+    
+    if user.nil?
+      render plain: 'User Not Found', status: 404 
+    elsif user.update(strong_params)
+      render json: user
+    else 
+      render json: user.errors.full_messages, status: 422
+    end 
+  end 
+  
+  def destroy 
+    user = User.find_by(id: id)
+    
+    if user.nil?
+      render plain: 'User Not Found', status: 404 
+    elsif user.destroy
+      redirect_to users_url
+    else 
+      render json: user.errors.full_messages, status: 422
+    end 
+      
   end 
   
   def strong_params 
     params.require(:user).permit(:name, :email)
+  end 
+  
+  def id 
+    params[:id]
   end 
   
 end 
--- first_routes/app/models/user.rb
+++ first_routes/app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  validates :name, :email, presence: true 
+  validates :name, presence: true 
+  validates :email, presence: true, uniqueness: true
 end 
